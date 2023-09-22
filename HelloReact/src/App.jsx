e02f4b26fbd1c9fe8ee6467b3ca3b8be8ea7f951
Sachin Gavali <sachingavali321@gmail.com>
2023-09-22 07:17:44 +0530

    DarkModeToggle

--- HelloReact/src/App.jsx
+++ HelloReact/src/App.jsx
@@ -1,9 +1,24 @@
-
+import { useState } from 'react'
+import { BsFillSunFill, BsFillMoonStarsFill } from 'react-icons/bs'
 const App = () => {
+  const [darkmode, setDarkMode] = useState(true);
+
+  const handleMode = () => {
+    setDarkMode(!darkmode);
+  }
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main className={`w-full h-screen  grid place-items-center ${darkmode == false ? 'bg-gray-100' : 'bg-gray-950 text-gray-100'}  `}>
+      <div className='grid container gap-10 mx-auto p-2 place-items-center '>
+
+        <h1 className={`text-center font-bold text-slate-800  text-lg lg:text-3xl  ${darkmode == false ? 'text-slate-900' : 'text-slate-100'} `} > Toggle {darkmode== false ? 'Dark' : 'Light'} Mode </h1>
+
+        {
+          darkmode == false ?  <BsFillSunFill className='icon' onClick={handleMode}  />  : <BsFillMoonStarsFill className='icon' onClick={handleMode}  /> 
+        }
+  
+ 
+   </div>
+  </main>
   )
 }
 
